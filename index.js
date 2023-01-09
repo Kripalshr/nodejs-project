398c44c205376a80dc0bcf062111881766b139f9
Kripal Shrestha <kripalshr@gmail.com>
2023-01-09 08:46:43 +0545

    javascript methods

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-let value = "Hello World!";
+// let value = "Hello World!";
 
-const newValue = "This is constant value";
-console.log(value);
-console.log(newValue);
+// const newValue = "This is constant value";
+// console.log(value);
+// console.log(newValue);
+
+let students = ['ram', 'shyam', 'hari'];
+console.log(`The first value of the array is ${students[students.length-1]}`); 
+students[0] = "someone";
+console.log(`The first value of the array after updating is ${students[0]}`);
+console.log(`The length of the array is ${students.length}`);
+
+students.push('Pukar')
+console.log(students);
+
+students.shift()
+console.log(students);
+
+console.log(students.length)
