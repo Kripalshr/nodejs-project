3da872d8f4b08ec6faab9ce74c749e844b6dc51c
Kripal Shrestha <kripalshr@gmail.com>
2023-01-11 08:35:06 +0545

    upto array loog(forEach)

--- index.js
+++ index.js
@@ -4,16 +4,39 @@
 // console.log(value);
 // console.log(newValue);
 
-let students = ['ram', 'shyam', 'hari'];
-console.log(`The first value of the array is ${students[students.length-1]}`); 
-students[0] = "someone";
-console.log(`The first value of the array after updating is ${students[0]}`);
-console.log(`The length of the array is ${students.length}`);
+// let students = ['ram', 'shyam', 'hari'];
+// console.log(`The first value of the array is ${students[students.length-1]}`); 
+// students[0] = "someone";
+// console.log(`The first value of the array after updating is ${students[0]}`);
+// console.log(`The length of the array is ${students.length}`);
 
-students.push('Pukar')
-console.log(students);
+// students.push('Pukar')
+// console.log(students);
 
-students.shift()
-console.log(students);
+// students.shift()
+// console.log(students);
 
-console.log(students.length)
+// console.log(students.length)
+
+// const getInfo = (name, age) => {
+//     return `My name is ${name} and my age is : ${age}`;
+//     // return "My name is " + name + " and my age is : " + age;
+
+// }
+// console.log(getInfo("Kripal", "21"));
+
+
+// const getFullName = (firstName, lastName, makeUpperCB) => {
+//     return makeUpperCB(`${firstName} ${lastName}`)
+// }
+
+// const makeUpper = (name) => {
+//     return name.toUpperCase( )
+// }
+
+// let fullName = getFullName("Kripal", "Shrestha", makeUpper);
+// console.log(fullName);
+
+const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+array.forEach((element, index) => console.log(element, index));
